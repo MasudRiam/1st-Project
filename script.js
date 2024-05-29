8459cf0d4d17791b69989f853b02a814c9c24028
MasudRiam <masudp.riam@gmail.com>
2024-05-30 02:30:53 +0600

    Add all important property done (Demo Done)

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('inquiryForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // add more form validation here if needed
+    alert('Form submitted successfully!');
+
+    // Reset the form after submission
+    document.getElementById('inquiryForm').reset();
+});
+
+
+
+// testimonial section 
